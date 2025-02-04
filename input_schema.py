92fa51a180a7947ec18b52887d58dcdfb0755938
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-02-04 18:41:24 +0530

    Update and rename input_schema.py to -

--- input_schema.py
+++
@@ -1,8 +0,0 @@
-INPUT_SCHEMA = {
-    "pdf_url": {
-        'datatype': 'STRING',
-        'required': True,
-        'shape': [1],
-        'example': ["https://arxiv.org/pdf/2412.19437"]
-    }
-}
